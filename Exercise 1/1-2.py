a065e526215eccf9f940e562282220c8e7ce312c
WesleyKasteller <wesleykcarlesso@edu.unifil.br>
2023-02-08 20:05:59 -0300

    Second Exercise

---
+++ Exercise 1/1-2.py
@@ -0,0 +1,38 @@
+f = open(r"C:\Users\ninja\Documents\Programas\Python\AOC\AdventureOfCode\Exercise 1\input.txt")
+
+list = f.readlines()
+
+firstMaxValue = 0
+secondMaxValue = 0
+thirdMaxValue = 0
+sum = 0
+
+for i in list:
+    line = i.split('\n')
+
+    if (line[0] != ''):
+        sum += int(i.split('\n')[0])
+    else:
+        if (sum > firstMaxValue):
+            thirdMaxValue = secondMaxValue
+            secondMaxValue = firstMaxValue
+            firstMaxValue = sum
+        if (sum > secondMaxValue and sum < firstMaxValue):
+            secondMaxValue = sum
+        if (sum > thirdMaxValue and sum < secondMaxValue):
+            thirdMaxValue = sum
+        sum = 0
+        
+if (sum > firstMaxValue):
+    thirdMaxValue = secondMaxValue
+    secondMaxValue = firstMaxValue
+    firstMaxValue = sum
+if (sum > secondMaxValue and sum < firstMaxValue):
+    secondMaxValue = sum
+if (sum > thirdMaxValue and sum < secondMaxValue):
+    thirdMaxValue = sum
+
+total = firstMaxValue + secondMaxValue + thirdMaxValue
+
+print('first:', firstMaxValue, ' second:', secondMaxValue, 'third:', thirdMaxValue)
+print('The sum of three highest values is', total)
